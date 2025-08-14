30506c015e339ab4540c149d8a8c74459bd21cd5
Mary <marshareeitumo@gmail.com>
2025-08-14 12:15:54 +0300

    adds lib

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,19 @@
+
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
+name_regex = re.compile(
+    r"^[A-Z](?:[a-z]+(?:['\-.][A-Za-z]+)*|(?:['\-.][A-Za-z]+)+)"
+    r"(?: [A-Z][a-z]*(?:['\-.][A-Za-z]+)*)*$"
+)
 
-name = r""
-name_regex = re.compile(name)
+phone_regex = re.compile(
+    r"^(?:\d{10}|\(\d{3}\)\s*\d{3}-\d{4}|\d{3}-\d{3}-\d{4})$"
+)
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
+email_regex = re.compile(
+    r"^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+)
 
-email_address = r""
-email_regex = re.compile(email_address)
+
+
+
